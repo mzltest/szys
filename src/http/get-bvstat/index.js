790ca721e817dd5d15605e2976bc98062640b37b
mzltest <52124882+mzltest@users.noreply.github.com>
2022-12-24 21:54:13 +0800

    Update index.js

--- src/http/get-bvstat/index.js
+++ src/http/get-bvstat/index.js
@@ -46,7 +46,7 @@ data=`<!DOCTYPE html>
 <p id="nextupdate"></p>
 <details><summary>历史数据</summary>{{DBFORE}}</details>
 <script>
-function average(array) {//封装求平均值函数
+function average(arr) {//封装求平均值函数
         var len = arr.length;
         var sum = 0;
         for(var i = 0;i<len;i++){
